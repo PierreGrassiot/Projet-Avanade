ddcd90b50a6241896cf39663bddb4ec3316d7bee
PierreGrassiot <pierre.grassiot@laposte.net>
2018-03-16 17:50:46 +0100

    Add 2 new scripts
    Add ValidateConstruction and DisplayModelParts scripts

---
+++ DisplayModelParts.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayModelParts : MonoBehaviour {
+
+    public List<GameObject> objList;                //liste d'objets à traiter
+    public int[] ordre;                             //ordre d'affichage à respecter
+
+    //map qui associe un ordre de placement à l'objet correspondant
+    private SortedDictionary<int, GameObject> objMap = new SortedDictionary<int, GameObject>();          
+
+    void Start () {
+
+        int i = 0;
+        foreach (GameObject obj in objList)
+        {
+            obj.SetActive(false);               //on masque chaque objet de la liste
+            objMap.Add(ordre[i], obj);          //on ajoute à la map le numéro d'apparition et l'objet de la liste
+            i++;
+        }
+
+        //on lance la routine Actions, définie ci-dessous
+        StartCoroutine(Actions());
+    }
+
+IEnumerator Actions()
+    {  
+        GameObject currentObj = new GameObject();
+
+        for (int i = 1; i < objList.Count + 1; ++i)         //autant de fois qu'il y a d'objets à traiter
+        {
+            objMap.TryGetValue(i, out currentObj);          //on récupère l'objet correspondant au numéro d'apparition suivant
+            currentObj.SetActive(true);                     //on l'affiche
+            yield return new WaitForSecondsRealtime(3);     //on attend 3 secondes
+        }
+    }
+}
---
+++ ValidateConstruction.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class ValidateConstruction : MonoBehaviour {  //script de validation de la constrution en comparant les coordonnées
+
+    public enum axes { x, y, z, rho, theta, phi };
+
+    public List<GameObject> objList;        //liste d'objets à traiter
+    public GameObject[] reference;          //tableau d'objets de référence
+
+    public Text result;                     //résultat écrit dans une chaîne de caractères
+    public float precision;                 //précision sur la comparaison
+    public axes axeChoisi;                  //axe à traiter
+
+    private string str;
+
+	void Update () {
+        str = "Running...\n";
+        int i = 0;
+        foreach (GameObject obj in objList)
+        {
+            ComparerObjets(obj, reference[i], axeChoisi);     //on compare chaque objet à traiter avec se référence
+            ++i;
+        }
+        result.text = str;
+    }
+
+    private void ComparerObjets(GameObject objCree, GameObject objRef, axes axeChoisi)
+    {
+        float compareCree = 0;
+        float compareRef = precision + 1;
+
+        switch(axeChoisi)             //selon l'axe choisi pour le traitement, on récupère les valeurs pour l'objet et sa référence
+        {
+            case axes.x :
+                compareCree = objCree.transform.position.x;
+                compareRef = objRef.transform.position.x;
+                break;
+            case axes.y :
+                compareCree = objCree.transform.position.y;
+                compareRef = objRef.transform.position.y;
+                break;
+            case axes.z :
+                compareCree = objCree.transform.position.z;
+                compareRef = objRef.transform.position.z;
+                break;
+            case axes.rho :
+                compareCree = objCree.transform.rotation.x;
+                compareRef = objRef.transform.rotation.x;
+                break;
+            case axes.theta :
+                compareCree = objCree.transform.rotation.y;
+                compareRef = objRef.transform.rotation.y;
+                break;
+            case axes.phi :
+                compareCree = objCree.transform.rotation.z;
+                compareRef = objRef.transform.rotation.z;
+                break;
+            default:
+                compareCree = precision + 1;
+                compareRef = 0;
+                break;
+        }
+
+        if (Math.Abs(compareCree - compareRef) < precision)
+        {
+            str += objCree.name + " " + " OK\n"; 
+            //on affiche le nom de l'objet s'il est bien construit
+        }
+    }
+}
